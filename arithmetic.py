7777d412326c4e7f4724e5657f9de214d6482244
AgneDJ <fizikee@gmail.com>
2023-11-29 14:58:05 -0800

    Functions added

--- arithmetic.py
+++ arithmetic.py
@@ -3,33 +3,47 @@
 
 def add(num1, num2):
     """Return the sum of num1 and num2."""
-
-    return 10
+    sum = num1 + num2
+    return sum
 
 
 def subtract(num1, num2):
     """Return the value of num1 minus num2."""
+    sub = num1 - num2
+    return sub
 
 
 def multiply(num1, num2):
     """Multiply the num1 by num2 and return the result."""
+    product = num1 * num2
+    return product
 
 
 def divide(num1, num2):
     """Divide the num1 by num2, returning a floating point."""
+    division = num1 / num2
+    return division
 
 
 def square(num1):
     """Return the square of num1."""
+    squared = num1 * num1
+    return squared
 
 
 def cube(num1):
     """Return the cube of num1."""
+    cube = num1 * num1 * num1
+    return cube
 
 
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
+    powered = num1 ** num2
+    return powered
 
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
+    modulus = num1 % num2  # percent symbol!
+    return modulus
